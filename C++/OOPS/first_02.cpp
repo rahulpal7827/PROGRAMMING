d53eed73aac4094d5ae91af0d2a5372f64e4f714
rahulpal <rahulpalr266@gmail.com>
2025-11-08 13:10:27 +0530

    ok

--- C++/OOPS/first_02.cpp
+++ C++/OOPS/first_02.cpp
@@ -1,5 +1,62 @@
-# include <iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    
-return 0;}
+class binary
+{
+    string s;
+
+public:
+    void read(void);
+    void chek_binary(void);
+    void comp(void);
+    void display();
+};
+
+void binary ::read(void)
+{
+    cout << "enter a binar number->" << endl;
+    cin >> s;
+}
+void binary ::comp(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else if (s.at(i) == '1')
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+void binary ::display()
+{
+    cout<<"\ndisplaying binary number:"<<endl;
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+}
+void binary ::chek_binary()
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "invalid binary number" << endl;
+            exit(0);
+        }
+    }
+}
+int main()
+{
+    binary a;
+    a.read();
+    a.chek_binary();
+    a.display();
+    a.comp();
+    a.display();
+    return 0;
+}
